a66041b5fbbd6822eec41ca7971173ac88ccf2bb
MoniqueBR1 <158618996+MoniqueBR1@users.noreply.github.com>
2025-10-28 13:46:24 -0400

    plays tone when any pin touched

--- pumpkin.py
+++ pumpkin.py
@@ -8,9 +8,9 @@ from time import sleep
 SAMPLE_RATE = 8000
 BYTES_PER_SAMPLE = 2
 
-sck_pin = Pin(14) # Serial clock (BCLK on breakout)
-ws_pin = Pin(13) # Word select (LRCLK on breakout)
-sd_pin = Pin(12) # Serial data (DIN on breakout)
+sck_pin = Pin(15) # Serial clock (BCLK on breakout)
+ws_pin = Pin(16) # Word select (LRCLK on breakout)
+sd_pin = Pin(17) # Serial data (DIN on breakout)
 
 t0 = TouchPad(Pin(11))
 t1 = TouchPad(Pin(12))
@@ -23,17 +23,19 @@ t7 = TouchPad(Pin(4))
 t8 = TouchPad(Pin(5))
 t9 = TouchPad(Pin(6))
 t10 = TouchPad(Pin(7))
-low0 = t0.read() + 500
-low1 = t1.read() + 500
-low2 = t2.read() + 500
-low3 = t3.read() + 500
-low4 = t4.read() + 500
-low5 = t5.read() + 500
-low6 = t6.read() + 500
-low7 = t7.read() + 500
-low8 = t8.read() + 500
-low9 = t9.read() + 500
-low10 = t10.read() + 500
+
+add_to_low = 5000
+low0 = t0.read() + add_to_low
+low1 = t1.read() + add_to_low
+low2 = t2.read() + add_to_low
+low3 = t3.read() + add_to_low
+low4 = t4.read() + add_to_low
+low5 = t5.read() + add_to_low
+low6 = t6.read() + add_to_low
+low7 = t7.read() + add_to_low
+low8 = t8.read() + add_to_low
+low9 = t9.read() + add_to_low
+low10 = t10.read() + add_to_low
 
 pins = [t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10]
 lows = [low0, low1, low2, low3, low4, low5, low6, low7, low8, low9, low10]
@@ -61,10 +63,10 @@ audio = I2S(0, # This must be either 0 or 1 for ESP32
             rate=8000,
             ibuf=10000)
 
-TONE_FREQ = 400
+tone_feq = 400
 AMPLITUDE = 3000
 
-n_samples = SAMPLE_RATE // TONE_FREQ
+n_samples = SAMPLE_RATE // tone_feq
 buffer_size = n_samples * BYTES_PER_SAMPLE
 
 buf = bytearray(buffer_size)
@@ -75,10 +77,10 @@ for i in range(n_samples):
     struct.pack_into("<h", buf, i*BYTES_PER_SAMPLE, sample)
 
 while True:
-    audio.write(buf)
     for i in range (0, NUM_TOUCH_PINS):
         if (pins[i].read()) > (lows[i]):
+            #tone_feq = NOTES[i]
             print(i, " pressed")
-        print(i, ": ", (pins[i]).read())
-    time.sleep(0.1)
+            audio.write(buf)
+        #print(i, ": ", (pins[i]).read())
 audio.deinit()
